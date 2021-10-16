7cabf741f4dc077288acd4f3905a12449ad07fdc
Yuri Pinzetta <pinzetta@gmail.com>
2021-10-16 19:26:25 -0300

    push pwa pt5

--- js/push.js
+++ js/push.js
@@ -1,13 +1,5 @@
 console.log('[Application] start push listening');
 
-const messaging = getMessaging();
-getToken(messaging, { vapidKey: 'BD6vuWn1zE6w0N9BNdW6j_OsRY16bCdGAzyZC7MvRr2AL-JAAAP3oYBBt78QSmFFzOn6ZSoQmHHvaohqnkbrKB4' }).then((currentToken) => {
-    if (currentToken) {
-        console.log(currentToken);
-        return currentToken;
-    } else {
-        console.log('No registration token available. Request permission to generate one.');
-    }
-}).catch((err) => {
-    console.log('An error occurred while retrieving token. ', err);
-});
+const messaging = app.messaging();
+const teste = messaging.getToken({vapidKey: "BD6vuWn1zE6w0N9BNdW6j_OsRY16bCdGAzyZC7MvRr2AL-JAAAP3oYBBt78QSmFFzOn6ZSoQmHHvaohqnkbrKB4"});
+console.log(teste)
