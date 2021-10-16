0a9ef4dc012530ec98fd5671beeffefeb30d431f
Yuri Pinzetta <pinzetta@gmail.com>
2021-10-16 18:53:48 -0300

    push pwa pt3

--- js/push.js
+++ js/push.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -19,7 +13,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app_firebase = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app_firebase);
-
 console.log('[Application] start push listening');
 
 const messaging = app_firebase.messaging();
